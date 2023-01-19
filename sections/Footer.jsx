0e9ff19a2f871e8b116d2cf6f537b83f59dbdab4
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2023-01-19 10:04:32 +0100

    18/01/2023

--- sections/Footer.jsx
+++ sections/Footer.jsx
@@ -58,7 +58,56 @@ const Footer = () => (
         />
       </div>
     </div>
-    <div className="hidden tablet:block">Footer Large</div>
+    <div className={`hidden tablet:block ${styles.paddings}`}>
+      <div className="flex justify-between my-14 h-[11rem]">
+        <div className="flex-col justify-between flex">
+          <div className="">
+            <Image
+              src={Logo}
+              alt="logo"
+              priority
+              style={{ filter: 'invert(100%)' }}
+              width={250}
+              height={250}
+              // blurDataURL="data:..." automatically provided
+              // placeholder="blur" // Optional blur-up while loading
+            />
+          </div>
+          <div className="flex justify-around">
+            <RiFacebookBoxFill className="w-[2.5rem] h-[2.5rem]" />
+            <AiFillYoutube className="w-[2.5rem] h-[2.5rem]" />
+            <AiOutlineTwitter className="w-[2.5rem] h-[2.5rem]" />
+            <BsInstagram className="w-[2.5rem] h-[2.5rem]" />
+            <BsPinterest className="w-[2.5rem] h-[2.5rem]" />
+          </div>
+        </div>
+        <div className="flex gap-[8rem]">
+          <div className="">
+            <ul className="flex flex-col justify-between h-full">
+              <li>Home</li>
+              <li>Pricing</li>
+              <li>Products</li>
+              <li>About Us</li>
+            </ul>
+          </div>
+          <div className="">
+            <ul className="flex flex-col gap-6">
+              <li>Careers</li>
+              <li>Community</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </div>
+        </div>
+        <div className="">
+          <div className="flex gap-5 items-center">
+            <div className=" bg-white text-gray-500 rounded-full flex justify-start py-4 pl-5 pr-24">
+              Updates in your box.....
+            </div>
+            <div className=" bg-orange-500 p-4 px-8 rounded-full">Go</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
